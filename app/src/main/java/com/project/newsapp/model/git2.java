d9869ef05886270fe8ba815c4ebafff154073b0c
nasim hridoy <nasimhridoy695@gmail.com>
2023-03-11 23:04:27 +0600

    git2.java is commited

---
+++ app/src/main/java/com/project/newsapp/model/git2.java
@@ -0,0 +1,18 @@
+package com.project.newsapp.model;
+
+public class git2 {
+        String dept;
+
+    public git2(String dept) {
+        this.dept = dept;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+}
+
