95bee4c30df7769e1f92a7a92f3365838e755791
nasim hridoy <nasimhridoy695@gmail.com>
2023-03-11 22:45:31 +0600

    git.java is commited

---
+++ app/src/main/java/com/project/newsapp/model/git.java
@@ -0,0 +1,27 @@
+package com.project.newsapp.model;
+
+public class git {
+    int roll;
+    String name;
+
+    public git(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
