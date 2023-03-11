6115610962b816920504ff540be3b008595163ca
nasim hridoy <nasimhridoy695@gmail.com>
2023-03-11 23:41:20 +0600

    com

--- app/src/main/java/com/project/newsapp/model/git2.java
+++ app/src/main/java/com/project/newsapp/model/git2.java
@@ -1,18 +1,18 @@
 package com.project.newsapp.model;
 
 public class git2 {
-        String dep;
+        String dept;
 
     public git2(String dept) {
-        this.dep = dept;
+        this.dept = dept;
     }
 
     public String getDept() {
-        return dep;
+        return dept;
     }
 
     public void setDept(String dept) {
-        this.dep = dept;
+        this.dept = dept;
     }
 }
 
